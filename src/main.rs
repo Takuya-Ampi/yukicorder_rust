b8167db0501a5adfc8179e8b407b88aff1332299
行比拓也 <imac@iMac.local>
2020-04-05 18:04:11 +0900

    pr 830

--- src/main.rs
+++ src/main.rs
@@ -13,5 +13,10 @@ fn read_vec<T: std::str::FromStr>() -> Vec<T> {
 
 fn main() {
     let num: i32 = read();
-    println!("{} {}",1 ,num);
+    let ans: f32 = num as f32 * 10 as f32;
+    println!("{}", ans);
 }
+
+// let num: i32 = read();
+// let ans: f32 = 1 as f32 / num as f32;
+// println!("{}", ans);
